7638f86e2cfe02a849a4fd0fd91c6937c86c797b
jewelCode <jewelbarua101@gmail.com>
2022-06-03 18:46:43 +0600

    project is updated

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ async function run() {
             res.send(product);
         })
         // Delete Product
+        // Delete Product
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
